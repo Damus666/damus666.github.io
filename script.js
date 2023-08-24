e9611beb78a4f7b79c5b5816d6a4ba488a5c2453
Damiano Ricciardi <macchia.cuordileone@gmail.com>
2023-08-24 22:51:33 +0200

    script and style not refreshed

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://damus666.github.io/style.css"/>`
-
+   
 const headerContent = /*html*/`
 <header class="main-header fs-2">
     <div class="align-h" style="width:80%;">
         <img class="main-pfp" src="https://damus666.github.io/mainpfp.jpg"  width="17%", height="17%">
         <h1><a class="clean-link" href="https://linktr.ee/damus666"  target="_blank" id="#title">Damus666</a></h1>  
-    </div>
-</header>
+    </div> 
+</header> 
 <div class="navigation">
     <div class="align-h align-self-v">
         <a class="clean-link fs-4 space-h" href="https://damus666.github.io">Home</a>
@@ -23,14 +23,15 @@ const headerContent = /*html*/`
         <p class="fs-4">|</p>
         <a class="clean-link fs-4 space-h" href="https://damus666.github.io/about">About</a>
     </div>
-</div>`
+</div>` 
 
 const footerContent = /*html*/`
 <div class="navigation">
     <div class="align-h align-self-v">
         <p class="p">This site was possible thanks to <a class="link-col"  target="_blank" href="https://pages.github.com">Github Pages</a></p>
     </div>
-</div>
+
+</div> 
 `
 
 document.getElementById("#head-cp").innerHTML = headContent
@@ -42,7 +43,7 @@ titleElement = document.getElementById("#title")
 const baseDocTitle = "Damus666"
 const baseTitle = "Damus666"
 const pageNames = ["Games","Projects","About"]
-
+  
 pageNames.forEach((page)=>{
     if (url.includes(page.toLowerCase())) {
         document.title = baseDocTitle+` - ${page}`
